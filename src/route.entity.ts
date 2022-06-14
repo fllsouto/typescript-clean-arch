c77da203c0cd4bca66ebff0047cad1e1a456a29b
Fellipe Souto <fllsouto@gmail.com>
2022-06-14 20:34:27 -0300

    step: creating a type props and ajusting code formatter

--- src/route.entity.ts
+++ src/route.entity.ts
@@ -1,17 +1,22 @@
-type LatLng = {lat: number, lng: number}
+export type LatLng = { lat: number; lng: number };
 
-class Route{
-    constructor(
-        public title: string,
-        public startPosition: LatLng,
-        public endPosition: LatLng,
-        public points: LatLng[]
-    ) {}
+export type RouteProps = {
+  title: string;
+  startPosition: LatLng;
+  endPosition: LatLng;
+  points: LatLng[];
+};
+
+class Route {
+  constructor(public props: RouteProps) {}
 }
 
-const rota = new Route('minha rota', 
-    {lat: 15, lng: 15},
-    {lat: 15, lng: 20},
-    [{lat: 15, lng: 15},
-    {lat: 15, lng: 20}],
-)
+const rota = new Route({
+  title: 'minha rota',
+  startPosition: { lat: 15, lng: 15 },
+  endPosition: { lat: 15, lng: 20 },
+  points: [
+    { lat: 15, lng: 15 },
+    { lat: 15, lng: 20 }
+  ]
+});
